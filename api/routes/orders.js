81ad2b7f1cad7dc49012df4b3ad548b53b999969
Maximilian Schwarzmüller <company@academind.com>
2017-11-28 00:42:11 +0100

    added a logger (morgan), nodemon and error handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Orders were fetched'
--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
